15d6c30919b3bf83416cd72fb42a2cb778c7887d
patrick <patkottman@gmail.com>
2017-02-13 21:31:15 +0100

    html css done

---
+++ js.js
@@ -0,0 +1,15 @@
+/**
+ * Created by patrick on 2/10/2017.
+ */
+
+ function instellingen(){
+    window.location.assign("Instellingen.html");
+}
+
+function home(){
+    window.location.assign("Main.html");
+}
+
+function voortgang(){
+    window.location.assign("Voortgang.html");
+}
