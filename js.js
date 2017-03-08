c82bfda08d6c7c6ec3fbb9dd010f1793878f6aa5
patrick kottman <patkottman@gmail.com>
2017-03-08 21:18:24 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -2,6 +2,14 @@
  * Created by patrick on 2/10/2017.
  */
 
+
+var totalEtc = 0;
+var etcPerMonth = 0;
+var monthsSinceStart = 7;
+var nodig = 0;
+var ectNodig = 50;
+var u = 0;
+
  function instellingen(){
     window.location.assign("Instellingen.html");
 }
@@ -13,3 +21,35 @@ function home(){
 function voortgang(){
     window.location.assign("Voortgang.html");
 }
+
+
+
+ document.getElementById('submit').onclick = function() {
+     for (i =1 ; i <= 12; i++) {
+         s = "e" + i;
+
+         if (document.getElementById(s).value != null) {
+
+             u = document.getElementById(s).value
+                 ? document.getElementById(s).value : 0;
+             u = parseInt(u);
+             totalEtc += u;
+
+             etcPerMonth = totalEtc / monthsSinceStart;
+
+            nodig = (ectNodig - totalEtc) / etcPerMonth;
+         }
+     }
+     document.getElementById('ect').innerHTML = totalEtc;
+     document.getElementById('ectmonths').innerHTML = etcPerMonth;
+     document.getElementById('months').innerHTML = nodig;
+
+     document.getElementById('popup').classList.remove('hidden');
+     totalEtc = 0;
+ };
+
+
+ document.getElementById('close').onclick = function () {
+
+    document.getElementById('popup').classList.add('hidden');
+ };
